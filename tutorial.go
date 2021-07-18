8174fdd2ab595ea92da5fc9da3f0803bff10cb84
Abhishek AN <darkabhi1520@gmail.com>
2021-07-18 18:17:09 +0530

    Implicit & explicit declaration
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- tutorial.go
+++ tutorial.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("HELLO WORLD")
+
+	number := "260"
+	var value bool
+	var anotherNumber uint64
+
+	println(anotherNumber)
+
+	println("Hello World!", value)
+
+	fmt.Printf("%T", number)
+
+	/**
+	0
+	Hello World! false
+	string
+	 */
 }
