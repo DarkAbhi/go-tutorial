6b25211ab291828f07332651d4cac2b7d2b6c21b
Abhishek AN <darkabhi1520@gmail.com>
2021-07-24 03:39:50 +0530

    Conditions & boolean expressions
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- tutorial.go
+++ tutorial.go
@@ -5,14 +5,7 @@ import (
 )
 
 func main() {
-	/*
-	Answer = 24
-	Answer = -8
-	*/
-	var num1 int = 28
-	var num2 int = 8
-	/*Answer = -8.000000*/
-	//answer:= num1 - float64(num2)
-	answer2:= num1%num2
-	fmt.Printf("Answer = %d", answer2)
+	x := 'A'
+	val := x<'b'
+	fmt.Printf("%t", val)
 }
